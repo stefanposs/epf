187a6ea45dba8776e6fca261efddf33b8f5c33a3
Stefan Poß <sp@stefanposs.de>
2021-08-02 21:02:18 +0200

    example app.ts

--- lib/bin/app.ts
+++ lib/bin/app.ts
@@ -25,3 +25,14 @@ console.log(event.getEvent());
 */
 dotenv.config({ path: '.env' })
 console.log(process.env.test)
+
+import { Logger } from "tslog";
+
+const log: Logger = new Logger({ name: "myLogger" });
+log.silly("I am a silly log.");
+log.trace("I am a trace log with a stack trace.");
+log.debug("I am a debug log.");
+log.info("I am an info log.");
+log.warn("I am a warn log with a json object:", {foo: "bar"});
+log.error("I am an error log.");
+log.fatal(new Error("I am a pretty Error with a stacktrace."))
