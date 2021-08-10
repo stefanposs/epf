0e43be7fe70df1100b7c2eab8715ae5c75129cac
Stefan Poß <sp@stefanposs.de>
2021-08-10 14:49:45 +0200

    google style guide

--- test/Event.test.ts
+++ test/Event.test.ts
@@ -1,9 +1,9 @@
-import { Event } from "../lib/models/Event"
+import {Event} from '../lib/models/Event';
 
-describe("Event", () => {
-  it("should return sum of two numbers", () => {
-    const event = new Event("kknkn","","")
+describe('Event', () => {
+  it('should return sum of two numbers', () => {
+    const event = new Event('kknkn', '', '');
 
-    expect(event.uuid).toStrictEqual("kknkn");
+    expect(event.uuid).toStrictEqual('kknkn');
   });
 });
